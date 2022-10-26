aa441ec84cda1de7e07241b957de8418b4d122fa
yungshun317 <seraph317@gmail.com>
2022-10-26 19:33:36 +0800

    [FEAT] Use Java-based container configuration for Spring

---
+++ src/main/java/yungshun/chang/springsecuritylms/config/LMSConfig.java
@@ -0,0 +1,26 @@
+package yungshun.chang.springsecuritylms.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages="yungshun.chang.springsecuritylms")
+public class LMSConfig {
+
+    // Define a bean for ViewResolver
+    @Bean
+    public ViewResolver viewResolver() {
+
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+
+        viewResolver.setPrefix("/WEB-INF/view/");
+        viewResolver.setSuffix(".jsp");
+
+        return viewResolver;
+    }
+}
